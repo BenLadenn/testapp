e041f3bb3cceb72de38771e7b6d1167f7a1d2d06
bekzat <bekzat46@mail.ru>
2021-11-15 15:31:16 +0600

    change2

--- Example.java
+++ Example.java
@@ -1,5 +1,5 @@
-class Example {
-    public static void main(String[] args){
-        System.out.println("Ffff");
+class Example{
+    public static void main(String args[]){
+        System.out.println("Prostaya programma na Java.");
     }
 }
